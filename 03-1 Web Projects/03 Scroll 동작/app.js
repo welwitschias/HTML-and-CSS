1f30d45cfc0587c967806340cc3349f779c7eaac
JunHyeok <108245248+welwitschias@users.noreply.github.com>
2022-07-14 15:57:50 +0900

    Add files via upload

---
+++ 03-1 Web Projects/03 Scroll 동작/app.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const boxes = document.querySelectorAll('.box');
+// 윈도우창에서 scroll하면 checkBoxes 함수 실행
+window.addEventListener('scroll', checkBoxes);
+// 처음 시작시 실행
+checkBoxes();
+
+function checkBoxes() {
+    // 박스를 보여주거나 사라지게 하는 트리거포인트 (4/5는 임의로 지정한 값)
+    const triggerBottom = (window.innerHeight / 5) * 4;
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top; //모든 박스의 상단 높이
+
+        if (boxTop < triggerBottom) {
+            box.classList.add('show'); // 박스 보이기(박스에 show class 달기)
+        } else {
+            box.classList.remove('show'); // 박스 안보이기(박스에 show class 삭제)
+        }
+    });
+}
---
+++ 03-1 Web Projects/04 Blur Filter/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0; // loading 숫자 초기값 0
+let int = setInterval(blurring, 30); // 0.03초(30ms)마다 blurring 함수 실행
+
+function blurring() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int); // loading 숫자가 100 이상이면 blurring 함수 종료
+    }
+
+    loadText.textContent = `${load}%`;
+    // loading 숫자가 0에서 100되는 동안 opacity가 1에서 0으로 변경
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    // loading 숫자가 0에서 100되는 동안 blur가 30에서 0으로 변경
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+// 한 레인지(0~100)를 다른 레인지(1~0), (30~0), ...로 변환하는 함수 공식
+function scale(number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
