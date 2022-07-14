a851f6bc290934efc97a1414e45cc44af7828173
JunHyeok <108245248+welwitschias@users.noreply.github.com>
2022-07-14 12:47:10 +0900

    Add files via upload

---
+++ 03-1 Web Projects/02 Hiding Search/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', function () {
+    search.classList.toggle('active');
+    input.focus();  // 검색창 입력커서가 나타나게 함
+});
